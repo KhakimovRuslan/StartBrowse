0b61e7f5d7b8aac315e5285047bce437410df2fd
KhakimovRuslan <xakimof@mail.ru>
2017-04-21 18:22:01 +0300

    Авторизация под администратором

---
+++ Login/Login/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Login
+{
+    [TestFixture]
+    public class LoginAsAdmin
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void startBrowse()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //driver.Manage().Timeouts().ImplicitlyWait((TimeSpan.FromSeconds(10)));
+        }
+        [Test]
+        public void Enter()
+        {
+            String Text = "admin";
+            driver.Url = "http://localhost:8081/litecard/admin";
+            driver.FindElement(By.Name("username")).SendKeys(Text);
+            driver.FindElement(By.Name("password")).SendKeys(Text);
+            driver.FindElement(By.TagName("button")).Click();
+            Thread.Sleep(3000);
+
+        }
+         [TearDown]
+          public void stop()
+          {
+            driver.Quit();
+            driver = null;
+          }
+
+
+      }
+}
+
