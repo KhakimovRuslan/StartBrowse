e013a892453c0b98887a7f98786cfbebfceee30e
KhakimovRuslan <xakimof@mail.ru>
2017-05-24 18:27:13 +0300

    Модификация грида, конечный тест

--- ClickAllElements_SeleniumGrid/ClickAllElements_SeleniumGrid/ClickAllElements_SeleniumGrid.cs
+++ ClickAllElements_SeleniumGrid/ClickAllElements_SeleniumGrid/ClickAllElements_SeleniumGrid.cs
@@ -12,52 +12,66 @@ namespace ClickAllElements_SeleniumGrid
     [TestFixture]
     public class ClickAllElements_SeleniumGrid
     {
-        private IWebDriver driver;
+        private IWebDriver driverIE;
         //private WebDriverWait wait;
+        private IWebDriver driverChrome;
 
         [SetUp]
-        public void Initialize()
-        {
-            DesiredCapabilities capabilities = DesiredCapabilities.InternetExplorer();
 
-            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capabilities);
+        public void Initialized()
+        {
+            Initialized(driverIE, DesiredCapabilities.InternetExplorer());
+            Initialized(driverChrome,DesiredCapabilities.Chrome());
+
+        }
+
+        private void Initialized(IWebDriver driver,DesiredCapabilities capabilities)
+        {
+
+
+            driver = new RemoteWebDriver(new Uri("http://192.168.209.53:4444/"), capabilities);
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Manage().Window.Maximize();
-
         }
+
         [Test]
         public void ClickAllElement_SeleniumGrid()
+        {
+            ClickAllElement_SeleniumGrid(driverIE);
+            ClickAllElement_SeleniumGrid(driverChrome);
+
+        }
+
+        private void ClickAllElement_SeleniumGrid(IWebDriver driver)
         {
             String Text = "admin";
             driver.Url = "http://192.168.209.53:8081/litecard/admin";
             driver.FindElement(By.Name("username")).SendKeys(Text);
             driver.FindElement(By.Name("password")).SendKeys(Text);
-            driver.FindElement(By.TagName("button")).Click();
+            driverIE.FindElement(By.TagName("button")).Click();
 
-            var rootAnchors = driver.FindElements(By.CssSelector("#app->a"));
+            var rootAnchors = driverIE.FindElements(By.CssSelector("#app->a"));
             for (int i = 0; i < rootAnchors.Count; i++)
             {
-                driver.FindElements(By.CssSelector("#app->a"))[i].Click();
-                driver.FindElement(By.TagName("h1"));
+                driverIE.FindElements(By.CssSelector("#app->a"))[i].Click();
+                driverIE.FindElement(By.TagName("h1"));
 
-                var firstLevelAnchors = driver.FindElements(By.CssSelector("ul.docs li:not(.selected) a"));
+                var firstLevelAnchors = driverIE.FindElements(By.CssSelector("ul.docs li:not(.selected) a"));
                 for (int j = 0; j < firstLevelAnchors.Count; j++)
                 {
-                    driver.FindElements(By.CssSelector("ul.docs li:not(.selected) a"))[j].Click();
-                    driver.FindElement(By.TagName("h1"));
+                    driverIE.FindElements(By.CssSelector("ul.docs li:not(.selected) a"))[j].Click();
+                    driverIE.FindElement(By.TagName("h1"));
                 }
 
             }
-
-
-
         }
+
         [TearDown]
         public void stop()
         {
-            driver.Quit();
-            driver = null;
+            driverIE.Quit();
+            driverIE = null;
         }
 
 
